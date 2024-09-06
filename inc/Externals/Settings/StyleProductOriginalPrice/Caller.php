33ce44ed5ab9a4518a52d88ca19751210adc2645
Trace <thien.nhatrangart@gmail.com>
2024-09-06 13:56:39 +0700

    Format code

--- inc/Externals/Settings/StyleProductOriginalPrice/Caller.php
+++ inc/Externals/Settings/StyleProductOriginalPrice/Caller.php
@@ -4,36 +4,32 @@ namespace BrandyBlocks\Externals\Settings\StyleProductOriginalPrice;
 
 use BrandyBlocks\Traits\SingletonTrait;
 
-class Caller
-{
-    use SingletonTrait;
+class Caller {
 
-    protected function __construct()
-    {
-        add_action('enqueue_block_editor_assets', array( $this, 'enqueue_settings_scripts' ));
-        add_action('wp_enqueue_scripts', array( $this, 'enqueue_frontend_scripts' ));
-        add_action('admin_enqueue_scripts', array($this,'enqueue_admin_scripts'));
-    }
+	use SingletonTrait;
 
-    public function enqueue_settings_scripts()
-    {
-        wp_enqueue_script('brandy-blocks/woo-product-original-price-controls', BRANDY_BLOCKS_PLUGIN_URL . 'inc/Externals/Build/Gutenberg/StyleProductOriginalPrice/index.js', array( 'wp-edit-post' ), time());
-        wp_localize_script(
-            'brandy-blocks/woo-product-original-price-controls',
-            'brandyGlobalSettings',
-            wp_get_global_settings()
-        );
-    }
+	protected function __construct() {
+		add_action( 'enqueue_block_editor_assets', array( $this, 'enqueue_settings_scripts' ) );
+		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_frontend_scripts' ) );
+		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_admin_scripts' ) );
+	}
 
-    public function enqueue_admin_scripts()
-    {
-        wp_enqueue_style('brandy-blocks/admin-original-price-settings-style', BRANDY_BLOCKS_PLUGIN_URL . 'inc/Externals/Settings/StyleProductOriginalPrice/style.css', array( 'wp-edit-post' ), time());
-    }
+	public function enqueue_settings_scripts() {
+		wp_enqueue_script( 'brandy-blocks/woo-product-original-price-controls', BRANDY_BLOCKS_PLUGIN_URL . 'inc/Externals/Build/Gutenberg/StyleProductOriginalPrice/index.js', array( 'wp-edit-post' ), time() );
+		wp_localize_script(
+			'brandy-blocks/woo-product-original-price-controls',
+			'brandyGlobalSettings',
+			wp_get_global_settings()
+		);
+	}
 
-    public function enqueue_frontend_scripts()
-    {
-        wp_enqueue_script('brandy-blocks/original-price-settings-script', BRANDY_BLOCKS_PLUGIN_URL . 'inc/Externals/Settings/StyleProductOriginalPrice/script.js', array( 'jquery' ), time());
-    }
+	public function enqueue_admin_scripts() {
+		wp_enqueue_style( 'brandy-blocks/admin-original-price-settings-style', BRANDY_BLOCKS_PLUGIN_URL . 'inc/Externals/Settings/StyleProductOriginalPrice/style.css', array(), time() );
+	}
+
+	public function enqueue_frontend_scripts() {
+		wp_enqueue_script( 'brandy-blocks/original-price-settings-script', BRANDY_BLOCKS_PLUGIN_URL . 'inc/Externals/Settings/StyleProductOriginalPrice/script.js', array( 'jquery' ), time() );
+	}
 }
 
 Caller::get_instance();
--- inc/Externals/Src/Gutenberg/StyleProductOriginalPrice/Edit.jsx
+++ inc/Externals/Src/Gutenberg/StyleProductOriginalPrice/Edit.jsx
@@ -1,62 +1,64 @@
-
-import CustomSettings from './components/CustomSettings';
+import CustomSettings from "./components/CustomSettings";
 
 /** Register attribute */
 function addWooProductPriceBlockAttribute(settings, name) {
-    if (settings.attributes == undefined) {
-        return settings;
-    }
-
-    if ("woocommerce/product-price" === name) {
-        settings.attributes = Object.assign(settings.attributes, {
-            originalPriceTypography: {
-                type: 'object',
-                default: {
-                    fontSize: 'default',
-                    color: 'default',
-                    appearance: {
-                        key: 'default',
-                        name: 'Default',
-                        style: {
-                            fontWeight: 'default',
-                            fontStyle: 'default'
-                        }
-                    },
-                }
-            }
-        });
-    }
-
+  if (settings.attributes == undefined) {
     return settings;
+  }
+
+  if ("woocommerce/product-price" === name) {
+    settings.attributes = Object.assign(settings.attributes, {
+      originalPriceTypography: {
+        type: "object",
+        default: {
+          fontSize: "",
+          color: "",
+          appearance: {
+            key: "default",
+            name: "Default",
+            style: {
+              fontWeight: "default",
+              fontStyle: "default",
+            },
+          },
+        },
+      },
+    });
+  }
+
+  return settings;
 }
 
 wp.hooks.addFilter(
-    "blocks.registerBlockType",
-    'brandy-blocks/woo-product-original-price-attributes',
-    addWooProductPriceBlockAttribute
+  "blocks.registerBlockType",
+  "brandy-blocks/woo-product-original-price-attributes",
+  addWooProductPriceBlockAttribute
 );
 
 /** Display controls */
 const StylePriceControls = wp.compose.createHigherOrderComponent(
-    (BlockEdit) => {
-        return (props) => {
-            const { Fragment } = wp.element;
-            const { attributes, setAttributes, name } = props;
-            return (
-                <Fragment>
-                    <BlockEdit {...props} />
-                    {("woocommerce/product-price" === name) &&
-                        <CustomSettings attributes={attributes} setAttributes={setAttributes} />
-                    }
-                </Fragment>
-            );
-        };
-    },
-    "stylePriceControls"
+  (BlockEdit) => {
+    return (props) => {
+      const { Fragment } = wp.element;
+      const { attributes, setAttributes, name } = props;
+      return (
+        <Fragment>
+          <BlockEdit {...props} />
+          {"woocommerce/product-price" === name && (
+            <CustomSettings
+              attributes={attributes}
+              setAttributes={setAttributes}
+            />
+          )}
+        </Fragment>
+      );
+    };
+  },
+  "stylePriceControls"
 );
 
 wp.hooks.addFilter(
-    "editor.BlockEdit",
-    'brandy-blocks/woo-product-original-price-controls',
-    StylePriceControls
+  "editor.BlockEdit",
+  "brandy-blocks/woo-product-original-price-controls",
+  StylePriceControls
 );
