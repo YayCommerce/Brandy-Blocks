40943ad3d12d2f43f838c4a48be347203c22eb64
Trace <thien.nhatrangart@gmail.com>
2024-09-13 11:22:22 +0700

    Remove render block hook in external Button settings

--- inc/Externals/Settings/ButtonSettings/Caller.php
+++ inc/Externals/Settings/ButtonSettings/Caller.php
@@ -12,7 +12,6 @@ class Caller {
 		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_frontend_scripts' ) );
 		add_action( 'enqueue_block_assets', array( $this, 'enqueue_frontend_scripts' ) );
 		add_filter( 'block_type_metadata_settings', array( $this, 'declare_attribute' ), 10 );
-		add_filter( 'render_block', array( $this, 'add_external_button_attributes' ), 10, 2 );
 	}
 
 	public function declare_attribute( $settings ) {
@@ -39,53 +38,6 @@ class Caller {
 
 	public function enqueue_frontend_scripts() {
 		wp_enqueue_style( 'brandy-blocks/button-settings-style', BRANDY_BLOCKS_PLUGIN_URL . 'inc/Externals/Settings/ButtonSettings/style.css', array(), time() );
-	}
-
-	public function add_external_button_attributes( $content, $block ) {
-		return $content;
-		// $attributes = (array) $block['attrs'];
-
-		// $escaped_data_attributes = array();
-		// $escaped_style           = '';
-
-		// foreach ( $attributes as $key => $value ) {
-		// 	if ( 'displayAnimation' !== $key ) {
-		// 		continue;
-		// 	}
-
-		// 	if ( empty( $value['type'] ) ) {
-		// 		continue;
-		// 	}
-
-		// 	if ( 'none' === $value['type'] ) {
-		// 		continue;
-		// 	}
-		// 	$escaped_data_attributes[] = 'data-animate-effect="' . esc_attr( $value['type'] ) . '"';
-		// 	$escaped_data_attributes[] = 'data-animate-on-view="' . esc_attr( $value['animateOnView'] ? 'true' : 'false' ) . '"';
-		// 	$escaped_style             = 'animation-duration:' . esc_attr( $value['duration'] ?? 3 ) . 's';
-		// }
-
-		// if ( ! empty( $escaped_style ) ) {
-		// 	$tags = new \WP_HTML_Tag_Processor( $content );
-		// 	if ( $tags->next_tag() ) {
-		// 		$existing_style = $tags->get_attribute( 'style' );
-		// 		$updated_style  = '';
-
-		// 		if ( empty( $existing_style ) ) {
-		// 			$existing_style = '';
-		// 		} else {
-		// 			if ( ! str_ends_with( $existing_style, ';' ) ) {
-		// 				$existing_style .= ';';
-		// 			}
-		// 		}
-		// 		$updated_style  = $existing_style;
-		// 		$updated_style .= $escaped_style . ';';
-		// 		$tags->set_attribute( 'style', $updated_style );
-		// 		$content = $tags->get_updated_html();
-		// 	}
-		// }
-
-		// return preg_replace( '/^<div /', '<div ' . implode( ' ', $escaped_data_attributes ) . ' ', trim( $content ) );
 	}
 
 }
